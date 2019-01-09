298c9159a59e1ae5fa60c410cf6ebc9369324e06
Joaquin Sanchez <sanchezjoaquin1995@gmail.com>
2019-01-09 09:48:17 -0300

    Error test

--- tests/lists.js
+++ tests/lists.js
@@ -7,32 +7,45 @@ require('./../models/card');
 
 describe('ListsController tests', () => {
   var Card = mongoose.model('card');
-  var CardMock = sinon.mock(Card);
 
   it('Should call find on cards model', done => {
-    let fake = {
-      find: function(params, callback) {
-        callback(null, 'RESULT');
-      }
-    };
-
-    let listsController = new ListsController(fake);
+    let spi = sinon.spy(Card, "find");
+    let listsController = new ListsController(Card);
 
     listsController.getAllCardsByListId('LIST', (err, cards) => {
-      assert.equal(cards, 'RESULT');
+      Card.find.restore();
+      
+      assert.equal(spi.called, true);
       done();
     });
   });
 
   it('Should find all cards by list', done => {
-    let listsController = new ListsController(Card);
-
+    let CardMock = sinon.mock(Card);
     CardMock.expects('find').withArgs({idList: 'LIST'}).once().yields(null, 'RESULT');
+
+    let listsController = new ListsController(Card);
 
     listsController.getAllCardsByListId('LIST', (err, cards) => {
       CardMock.verify(); // verifies expectations
       CardMock.restore(); // restores all overriden methods and properties
+
+      assert.equal(err, null);
       assert.equal(cards, 'RESULT');
+      done();
+    });
+  });
+
+  it('Should return error finding all cards by list', done => {
+    let CardMock = sinon.mock(Card);
+    CardMock.expects('find').withArgs({idList: 'LIST'}).once().yields('ERROR', null);
+    let listsController = new ListsController(Card);
+
+    listsController.getAllCardsByListId('LIST', (err, cards) => {
+      CardMock.verify();
+      CardMock.restore();
+
+      assert.equal(err, 'ERROR');
       done();
     });
   });
